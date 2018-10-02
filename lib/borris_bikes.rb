0194354c4ab7d590f769065ddb629c3776b28974
Caitlin Cooling <ccooling97@gmail.com>
2018-10-02 16:52:30 +0100

    Write docking test

--- lib/borris_bikes.rb
+++ lib/borris_bikes.rb
@@ -1,7 +1,11 @@
-require_relative 'Bikes.rb'
-
+require_relative 'Bikes'
 class DockingStation
+  attr_reader :bike
+  def initialize(bike)
+  end
   def release_bike
     Bike.new
+  end
+  def dock_bike
   end
 end
--- spec/borris_bikes_spec.rb
+++ spec/borris_bikes_spec.rb
@@ -1,9 +1,10 @@
 require 'borris_bikes'
   describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike }
   it 'releases a bike and expects it to work' do
      station = DockingStation.new
      bike = station.release_bike
      expect(bike.working?).to eq true
    end
+     it { is_expected.to respond_to :dock_bike }
 end
