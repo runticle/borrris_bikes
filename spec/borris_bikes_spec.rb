1cd837e796d5e236236f6eabbf83da54b9210462
Muna <mmhussein125@gmail.com>
2018-10-02 14:29:59 +0100

    added test

--- spec/borris_bikes_spec.rb
+++ spec/borris_bikes_spec.rb
@@ -1,4 +1,7 @@
 require 'borris_bikes'
 RSpec.describe DockingStation do
-
+  it 'bike returns true for release_bike method' do
+    bike = DockingStation
+    expect(bike.release_bike).to eq true
+  end
 end
