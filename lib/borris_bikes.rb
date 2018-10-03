95251e188d459e598e4d3a3bc8f2e015b20a30f1
Caitlin Cooling <ccooling97@gmail.com>
2018-10-03 16:11:25 +0100

    Raise exception if no bikes

--- lib/borris_bikes.rb
+++ lib/borris_bikes.rb
@@ -1,16 +1,22 @@
 require_relative 'Bikes'
 
 class DockingStation
+  attr_reader :bike
   def initialize()
     @station = []
   end
   def release_bike
-    bike = Bike.new
+    if @station.empty?
+      raise "Oops, there are no bikes here"
+    else
+      @bike
+    end
   end
   def dock(bike)
     @bike = bike
     @station.push(@bike)
   end
-  attr_reader :bike
-
+  # def bike_available?
+  #
+  # end
 end
--- spec/borris_bikes_spec.rb
+++ spec/borris_bikes_spec.rb
@@ -1,16 +1,34 @@
 require 'borris_bikes'
   describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it 'releases a bike and expects it to work' do
-     station = DockingStation.new
-     bike = station.release_bike
-     expect(bike.working?).to eq true
-   end
-     it { is_expected.to respond_to(:dock).with(1).argument }
-     it { is_expected.to respond_to(:bike) }
-     it 'returns bike' do
-       bike = Bike.new
-       subject.dock(bike)
-       expect(subject.bike).to eq bike
+
+
+
+
+
+  describe '#release_bike' do
+    it 'releases a bike if one is avaiable' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
      end
+     it 'raises error if no bikes are available' do
+       expect { subject.release_bike }.to raise_error "Oops, there are no bikes here"
+     end
+ end
+
+
+
+#     it { is_expected.to((bike.working?).to eq true }
+#  write a test to test for true
+
+   it { is_expected.to respond_to(:dock).with(1).argument }
+   it { is_expected.to respond_to(:bike) }
+   it 'returns bike' do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.bike).to eq bike
+   end
+   # it { is_expected.to respond_to(:bike_available?) }
+
 end
