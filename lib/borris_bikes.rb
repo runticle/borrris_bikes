4c33a282fabe60df4b4c0493d72dfba24a970529
NaomiBid <naomi0bid@gmail.com>
2018-10-04 14:43:51 +0100

    created full? and empty? private methods

--- lib/borris_bikes.rb
+++ lib/borris_bikes.rb
@@ -5,21 +5,33 @@ class DockingStation
   def initialize()
     @station = []
   end
+
   def release_bike
-    if @station.empty?
+    if empty?
       raise "Oops, there are no bikes here"
     else
       @station[0]
     end
   end
+
   def dock(bike)
     @station.count
-    if @station.count <= 19
+    if full?
        @station.push(bike)
       "Bike docked"
     else
        @station
       raise "Station full"
     end
+  end
+
+  private
+
+  def full?
+    @station.count <= 19
+  end
+
+  def empty?
+    @station.empty?
   end
 end
