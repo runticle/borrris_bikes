a657f598dd36e15ad8e41c16b0a21f3476a907dc
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-05 16:22:15 +0100

    refactored collect_from into three SRP-compliant methods

--- lib/van.rb
+++ lib/van.rb
@@ -4,10 +4,9 @@ class Van
   end
 
   def collect_from(location, working)
-    raise "#{location.class} does not have the bikes you're looking for" if location.bikes.select {|bike| bike.working? == working}.empty?
-    chosen_bike = location.bikes.find { |bike| bike.working? == working }
-    @bikes << chosen_bike
-    location.bikes.delete(chosen_bike)
+    raise "#{location.class} does not have the bikes you're looking for" unless has_right_bikes?(location, working)
+    @bikes << chosen_bike(location, working)
+    location.bikes.delete chosen_bike(location, working)
   end
 
   def deliver_to(location, working)
@@ -17,6 +16,16 @@ class Van
     location.bikes << chosen_bike
     @bikes.delete(chosen_bike)
   end
+
+
+  def has_right_bikes?(location, working)
+    location.bikes.select {|bike| bike.working? == working}.any?
+  end
+
+  def chosen_bike(location, working)
+    location.bikes.find { |bike| bike.working? == working }
+  end
+
 
   attr_accessor :bikes
 end
